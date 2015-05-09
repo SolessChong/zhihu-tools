9fdb067ddb1881eb8ea50f2f44651caebaf7f0ae
solesschong <solesschong@gmail.com>
2015-05-10 01:41:39 +0800

    use python

---
+++ zhihu-plugin/contentscript.js
@@ -0,0 +1,9 @@
+var script = document.createElement('script');
+script.setAttribute("type", "text/javascript");
+script.setAttribute("async", true);
+script.setAttribute("src", chrome.extension.getURL("require-jquery.js"));  
+//Assuming your host supports both http and https
+var head = document.head || document.getElementsByTagName( "head" )[0] || document.documentElement;
+head.insertBefore(script, head.firstChild);
+
+alert('1');
